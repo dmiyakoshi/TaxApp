c618612b4f9d7cb1b7732b124f43549f79592828
dmiyakoshi <d1m354dai@gmail.com>
2021-11-09 20:51:10 +0900

    達成すべき要件終了

---
+++ TaxApp/TaxViewController.swift
@@ -0,0 +1,107 @@
+//
+//  TaxViewController.swift
+//  TaxApp
+//
+//  Created by 宮越大輝 on 2021/11/08.
+//
+
+import UIKit
+
+class TaxViewController: UIViewController {
+    
+    @IBOutlet weak var displatPriceLbel: UILabel!
+    @IBOutlet weak var priceTableView: UITableView!
+    @IBOutlet weak var textFiled: UITextField!
+    
+    var inputPrice: Int = 0
+    var selectedTax: Float = 1.1
+    var priceArray: [String] = []
+    var totalPrice: Float = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        priceTableView.dataSource = self
+        textFiled.keyboardType = UIKeyboardType.numberPad //textfiledには数字(Int)のみ入力可能
+        displatPriceLbel.text = ""
+    }
+    
+    @IBAction func selectTaxSegment(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0 {
+            selectedTax = 1.08
+        } else if sender.selectedSegmentIndex == 1 {
+            selectedTax = 1.1
+        } else {
+            // none
+        }
+        showPrice()
+    }
+    
+    @IBAction func addButton(_ sender: Any) {
+        if textFiled.text != nil {
+            priceArray.append("\(Float(inputPrice) * selectedTax)") //cellに入れるためにString型
+            totalPrice += Float(inputPrice) * selectedTax
+            initPrice()
+            
+            let userDefault = UserDefaults.standard
+            userDefault.set(totalPrice, forKey: "sum")
+        } else {
+            // none
+        }
+        
+        priceTableView.reloadData()
+    }
+    
+    @IBAction func changePrice(_ sender: UITextField) {
+        showPrice()
+    }
+    
+    func showPrice() {
+        if textFiled.text != nil {
+            inputPrice = textFiled.textToInt //extensioのtextToIntでInt型に変換しています
+            let priceInTax = Float(inputPrice) * selectedTax
+            
+            displatPriceLbel.text = "\(priceInTax)"
+        } else {
+            inputPrice = 0
+        }
+    }
+    
+    func initPrice() {
+        displatPriceLbel.text = ""
+        textFiled.text = ""
+        inputPrice = 0
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        //この画面に戻ってきたら値は初期化していく
+        initPrice()
+        priceArray = []
+        totalPrice = 0
+        let userDefault = UserDefaults.standard
+        userDefault.set(totalPrice, forKey: "sum")
+        priceTableView.reloadData()
+    }
+}
+
+extension UITextField {
+    var textToInt: Int {
+        let text = self.text
+        let int = text
+            .flatMap{Int($0)} ?? 0
+        return int
+    }
+}
+
+extension TaxViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return priceArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PriceCell", for: indexPath)
+        
+        cell.textLabel?.text = priceArray[indexPath.row]
+        
+        return cell
+    }
+}
--- TaxApp/ViewController.swift
+++ TaxApp/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var sumPrice: Int = 0
 
+    @IBOutlet weak var displayLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let userDefault = UserDefaults.standard
+        
+        if userDefault.object(forKey: "sum") != nil {
+            sumPrice = userDefault.object(forKey: "sum") as! Int
+        }
+        displayLabel.text = "\(sumPrice)"
     }
 
-
+//    self.navigationController?.popViewController(animated: true)
 }
 
